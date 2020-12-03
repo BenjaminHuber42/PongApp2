afcb31ff9e986285c7d4b9e7e0abdc7a652ec5c5
BenjaminHuber42 <70213886+BenjaminHuber42@users.noreply.github.com>
2020-12-03 08:13:56 -0700

    Initial commit

---
+++ src/pongApp/controller/PongController.java
@@ -0,0 +1,10 @@
+package pongApp.controller;
+
+public class PongController
+{
+
+	public void start ()
+	{
+		
+	}
+}
---
+++ src/pongApp/controller/runner.java
@@ -0,0 +1,12 @@
+package pongApp.controller;
+
+
+public class runner
+{
+	public static void main (String [] args) 
+	{
+		PongController app = new PongController();
+		app.start();
+	}
+}
+,
